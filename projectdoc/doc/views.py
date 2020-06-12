b5e91990f0a99e494a3dea748f576b69d42c63da
Vibin Marish <35630389+vibinmarish@users.noreply.github.com>
2020-06-13 03:13:47 +0530

    User registration module added

---
+++ projectdoc/doc/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+
+    if request.method =="POST":
+        form=UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user=form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New Account Created: {username}")
+            login(request,user)
+            return redirect("home")
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
+    form= UserCreationForm
+    return render(request,"register.html",context={"form":form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request,"Logged Out Successfully !")
+    return redirect("home")
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get('username')
+            password =form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                messages.info(request,f"You are now logged in as {username}")
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+    form =AuthenticationForm()
+    return render(request,"login.html",
+                  {"form":form})
---
+++ projectdoc/projectdoc/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.contrib.auth import login
+from django.urls import path
+from doc import views
+from django.contrib.auth.views import LoginView
+app_name="doc"
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",views.home,name='home'),
+    path("register/",views.register,name='register'),
+    path("register/home/",views.home,name='homereg'),
+    path("login/home/",views.home,name='homelog'),
+    path("logout/", views.logout_request, name="logout"),
+    path("login/",views.login_request,name='login'),
+    ]
